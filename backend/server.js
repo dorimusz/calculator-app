303100d919ddb43a89e28bf75b02de8baa7a0da3
dorimusz <78429829+dorimusz@users.noreply.github.com>
2022-06-22 22:59:19 +0200

    added dotenv

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
+require("dotenv").config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
