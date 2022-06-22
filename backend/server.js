306390442aa6281b59966178b78ab0e57191be7e
dorimusz <78429829+dorimusz@users.noreply.github.com>
2022-06-22 22:56:54 +0200

    working backend with test route

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+const testRoute = require('./routes/testroute');
+app.use('/api', testRoute)
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
