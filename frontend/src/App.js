908965f80b2d21cfa0a0046fe688713de626bb2f
dorimusz <78429829+dorimusz@users.noreply.github.com>
2022-06-22 22:42:26 +0200

    clean & working react app

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import http from 'axios'
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Hello, world!</h1>
+    </div>
+  );
+}
+
+export default App;
