718891c5ba0e31d41e273329a3705ec715ddbfeb
dorimusz <78429829+dorimusz@users.noreply.github.com>
2022-06-23 10:20:36 +0200

    clickable numpad, result on display

--- frontend/src/components/Calculator.jsx
+++ frontend/src/components/Calculator.jsx
@@ -1,35 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/Calculator.css'
 
 const Calculator = () => {
+    const [result, setResult] = useState("");
+    const [display, setDisplay] = useState("");
+
     const createNumbers = () => {
         const numbers = [];
 
         for (let i = 1; i < 10; i++) {
-            numbers.push(<button key={i}>{i}</button>)
+            numbers.push(<button onClick={() => updateDisplay(i)} key={i}>{i}</button>)
         }
 
         return numbers;
+    };
+
+    const updateDisplay = (clickedButton) => {
+        setDisplay(display + clickedButton);
+        console.log(display);
+        console.log(clickedButton);
     }
     return (
         <div className='calculator'>
             <div className='display'>
-                <span className='displayOnTheGo'>(0)</span>0
+                <p className='displayOnTheGo'>(0)</p>
+                {display || "0"}
             </div>
 
             <div className='operators'>
                 <button>C</button>
-                <button>+</button>
-                <button>-</button>
-                <button>*</button>
-                <button>/</button>
+                <button onClick={() => updateDisplay('+')}>+</button>
+                <button onClick={() => updateDisplay('-')}>-</button>
+                <button onClick={() => updateDisplay('*')}>*</button>
+                <button onClick={() => updateDisplay('/')}>/</button>
                 <button>=</button>
             </div>
 
             <div className='numbers'>
                 {createNumbers()}
-                <button className='decimal'>.</button>
-                <button>0</button>
+                <button onClick={() => updateDisplay('.')}>.</button>
+                <button onClick={() => updateDisplay('0')}>0</button>
             </div>
 
             <div className='calcMemory'>
