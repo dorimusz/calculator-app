36a90d7aee7e826988fdf7ed0e7bf4208a04dd57
dorimusz <78429829+dorimusz@users.noreply.github.com>
2022-06-24 16:36:30 +0200

    req: missing credentials, body

--- backend/routes/memoryRoutes.js
+++ backend/routes/memoryRoutes.js
@@ -2,20 +2,23 @@ const router = require('express').Router();
 const fileSystem = require('fs');
 
 router.post('/addMem', (req, res) => {
-    fileSystem.readFile('./memory/memory.json', function (err, data) {
-        let memoryArray = JSON.parse(data)
-        console.log(memoryArray, "array, line 10")
-        memoryArray.push(req.body);
-        console.log(memoryArray, "memory array var")
-        fileSystem.writeFileSync('./memory/memory.json', JSON.stringify(memoryArray))
-    })
+    if (!req.body) return res.sendStatus(400);
+    if (!req.body?.calculation || !req.body?.result || !req.body?.timestamp) return res.sendStatus(400);
 
-    res.sendStatus(200).json("Added to memory")
+    fileSystem.readFile('./memory/memory.json', function (err, data) {
+        let memoryArray = JSON.parse(data);
+        console.log(memoryArray, "array, line 10");
+        memoryArray.push(req.body);
+        console.log(memoryArray, "memory array var");
+        fileSystem.writeFileSync('./memory/memory.json', JSON.stringify(memoryArray));
+    });
+
+    res.sendStatus(200).json("Added to memory");
 })
 
 
 router.get('/readMem', (req, res) => {
-    res.send('Hello World on new route!')
+    res.send('Hello World on new route!');
 })
 
 module.exports = router;
