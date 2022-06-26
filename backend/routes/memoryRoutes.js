2a2e63d433bd0dc6fc8cae2e82009deba4b05558
dorimusz <78429829+dorimusz@users.noreply.github.com>
2022-06-26 21:05:23 +0200

    checking if file exists, creating if not

--- backend/routes/memoryRoutes.js
+++ backend/routes/memoryRoutes.js
@@ -1,23 +1,43 @@
 const router = require('express').Router();
 const fileSystem = require('fs');
+const path = './memory/memory.json';
 
-router.post('/addMem', (req, res) => {
+router.post('/memory', (req, res) => {
     if (!req.body) return res.sendStatus(400);
     if (!req.body?.calculation || !req.body?.result || !req.body?.timestamp) return res.sendStatus(400);
 
-    fileSystem.readFile('./memory/memory.json', function (err, data) {
-        let memoryArray = JSON.parse(data);
-        console.log(memoryArray, "array, line 10");
-        memoryArray.push(req.body);
-        console.log(memoryArray, "memory array var");
-        fileSystem.writeFileSync('./memory/memory.json', JSON.stringify(memoryArray));
-    });
+    const writeFile = () => {
+        //readFile is async, keeping the server async 
+        fileSystem.readFile(path, (err, data) => {
+            console.log(err) //file does not exist
+            if (err) return res.sendStatus(500); // error occured
+            let memoryArray = JSON.parse(data);
+            memoryArray.push(req.body);
+            console.log(typeof (memoryArray))
+            fileSystem.writeFile(path, JSON.stringify(memoryArray), (err, result) => {
+                if (err) return res.sendStatus(500); // error occured;
+            });
+        });
+    }
 
-    res.sendStatus(200).json("Added to memory");
+    //not recommended to use .access() / .exists() before readFile or any kind of modification
+    const isFile = fileSystem.existsSync(path); //stops everything until returns w boolean answer
+
+    if (isFile) {
+        writeFile();
+    } else {
+        fileSystem.appendFile(path, '[]', function (err) {
+            if (err) return res.sendStatus(500); // error occured
+
+        });
+        writeFile();
+    }
+
+    res.status(200).json("Equation added to memory");
 })
 
 
-router.get('/readMem', (req, res) => {
+router.get('/memory', (req, res) => {
     res.send('Hello World on new route!');
 })
 
